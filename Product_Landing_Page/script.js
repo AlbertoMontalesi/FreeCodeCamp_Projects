db13fed1e2c7ceb93b5fdc96f08da5d53fb89d50
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-07-18 17:17:05 +0700

    add mobile navbar

--- Product_Landing_Page/script.js
+++ Product_Landing_Page/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+  //fullpage scroll
   $('#fullpage').fullpage();
   anchors:['landing-page', 'features', 'how_it_works','pricing']
+
+
+  //materialize navbar
+  $(".button-collapse").sideNav();
 });
