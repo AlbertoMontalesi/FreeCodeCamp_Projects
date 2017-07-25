baa43ebbe69448df1bdc2e4c505f6302fd21b400
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-07-25 17:41:30 +0700

    complete first version

--- Product_Landing_Page/script.js
+++ Product_Landing_Page/script.js
@@ -1,10 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   //fullpage scroll
-  $('#fullpage').fullpage();
-  anchors:['landing-page', 'features', 'how_it_works','pricing']
+  $('#fullpage').fullpage({
+    
+    anchors: ['landing-page', 'features', 'how_it_works', 'pricing'],
+    scrollOverflowReset: true
+  });
+
+  
 
 
-  //materialize navbar
-  $(".button-collapse").sideNav();
+
+
+    //materialize navbar
+    $(".button-collapse").sideNav();
 });
