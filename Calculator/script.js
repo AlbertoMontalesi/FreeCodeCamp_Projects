92c5088aeaee5757b0a8228d014eabf244cb26c8
albertomontalesi <alberto.montalesi@outlook.it>
2018-05-03 12:23:35 +0700

    complete first version of calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,13 +3,49 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let buttons = document.querySelectorAll("button");
 let display = document.querySelector("#display");
+let display_operation = document.querySelector("#display_operation");
+let display_result = document.querySelector("#display_result");
 
+
+let calculation = "";
+let equal = document.querySelector("#equal");
+
+// display the buttons that are clicked
 buttons.forEach( button => {
   button.addEventListener("click", () => {
-    console.log(button.innerHTML);  
-    display.innerHTML += button.innerHTML;
+    // if user click equal calculate the operation
+    if(button.innerHTML === "=") {
+      calculateResult(calculation);
     }
-  )
+    // if user click clear, delete everything
+    else if (button.id === "clear"){
+      calculation = "";
+      display_operation.innerHTML = calculation;
+      display_result.innerHTML = "";
+    }
+    // if user click delete, remove last value
+    else if (button.id === "delete"){
+      calculation = calculation.slice(0,-1);
+      display_operation.innerHTML = calculation;
+      calculateResult(calculation);
+      
+    }
+    // if user click numbers or operators just display them
+    else  {
+      calculation += button.innerHTML;
+      display_operation.innerHTML = calculation;
+      return calculation;
+    }
+    }
+  );
 });
+
+//calculateResult function
+function calculateResult(string){
+  display_result.innerHTML = eval(string);
+  if (display_result.innerHTML === "undefined") {
+    display_result.innerHTML = "";
+  }
+}
 
 });
